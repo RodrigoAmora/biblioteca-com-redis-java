cd4863bdbc0256339c21c3783de667fbec5b0364
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2026-04-18 01:39:06 -0300

    Classe de modelo

---
+++ src/main/java/br/com/rodrigoamora/bibliotecacomredis/model/Livro.java
@@ -0,0 +1,29 @@
+package br.com.rodrigoamora.bibliotecacomredis.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "livros")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Livro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String autor;
+
+    private String genero;
+
+    private Integer qtdPaginas;
+
+}
