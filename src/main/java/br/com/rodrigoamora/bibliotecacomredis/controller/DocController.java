10fa1896478caad7f33a995bfcb8903c46ef2950
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2026-04-18 01:47:27 -0300

    Rotas de documentação

---
+++ src/main/java/br/com/rodrigoamora/bibliotecacomredis/controller/DocController.java
@@ -0,0 +1,20 @@
+package br.com.rodrigoamora.bibliotecacomredis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class DocController {
+
+    @GetMapping("/swagger")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui.html");
+    }
+
+    @GetMapping("/redoc")
+    public RedirectView redirectRedoc() {
+        return new RedirectView("/redoc.html");
+    }
+
+}
