8feb977ac27b6a8fff554bb6ec81d86b9b6731f5
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2026-04-18 01:45:01 -0300

    API de livro

---
+++ src/main/java/br/com/rodrigoamora/bibliotecacomredis/controller/LivroController.java
@@ -0,0 +1,33 @@
+package br.com.rodrigoamora.bibliotecacomredis.controller;
+
+import br.com.rodrigoamora.bibliotecacomredis.dto.LivroDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "API de Livro")
+@RestController
+@RequestMapping("/api/livro")
+public class LivroController {
+
+
+    @Operation(summary = "Busca de livro")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Buscar livro pelo id",
+                    content = @Content(schema = @Schema(implementation = LivroDto.class))
+            ),
+    })
+    public ResponseEntity<LivroDto> buscarLivroPeloId(Long id) {
+        return null;
+    }
+
+
+}
